371897322ea74c55398ccebd11cd1d015563112b
jerlingo527 <jlingo2@student.rcc.edu>
2016-03-02 22:04:01 -0800

    Signed-off-by: jerlingo527 <jlingo2@student.rcc.edu>

---
+++ Lab/Lab 02-17-16/HelloWorld_V1/main.cpp
@@ -0,0 +1,34 @@
+/* 
+ * File:   main.cpp
+ * Author: Jericho Lingo
+ * Created on February 17, 2016, 9:45 AM
+ * Purpose: Our first program
+ */
+
+//System Libraries
+#include <iostream>
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution Begins Here!
+int main(int argc, char** argv) {
+    //Declare variables
+    
+    //Initialize variables
+    
+    //Input values
+    
+    //Map the inputs to the outputs
+    
+    //Output the results
+    cout<<"Hello World"<<endl;
+    
+    //Exit Stage Right!
+    return 0;
+}
+
