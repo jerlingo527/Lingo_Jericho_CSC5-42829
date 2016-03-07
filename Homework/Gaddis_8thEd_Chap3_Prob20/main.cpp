1c981ce7c0ca6eeb28780a81da43cc7e00be4615
jerlingo527 <jlingo2@student.rcc.edu>
2016-03-07 10:55:30 -0800

    Signed-off-by: jerlingo527 <jlingo2@student.rcc.edu>

---
+++ Homework/Gaddis_8thEd_Chap3_Prob20/main.cpp
@@ -0,0 +1,39 @@
+/* 
+ * File:   main.cpp
+ * Author: Jericho Lingo
+ * Created on March 7, 2016, 10:50 AM
+ * Purpose: Pizza
+ */
+
+//System Libraries
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+//User Libraries
+
+
+//Global Constants
+const PI = Atan(1.0) * 4;
+const Area = PId^2/4
+
+int main(int argc, char** argv) {
+    //Declare variables
+    
+    
+    //Initialize variables
+    
+    
+    //Input values
+    
+    
+    //Map the inputs to the outputs
+    
+    
+    //Output the results
+    
+    
+    //Exit Stage Right!
+    return 0;
+}
+
